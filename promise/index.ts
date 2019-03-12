7e99eb50293b71b11f6a37a0b2c32833671a0265
Feiyang1 <plane1113@gmail.com>
2019-03-11 22:03:27 -0700

    resolve a value in basic case

--- promise/index.ts
+++ promise/index.ts
@@ -6,27 +6,84 @@ enum State {
 }
 
 interface Executor {
-    (resolve: (value: any) => void, reject: (value: any) => void): void
+    (resolve: (value: any) => void, reject: (value: any) => void): any
+}
+
+interface CallBack {
+    (value: any): any
+}
+
+class Subscriber {
+    private isCalled = false;
+    constructor(private resolveCb?: CallBack, private rejectCb?: CallBack) {
+
+    }
+
+    callResolveAsync(val: any) {
+        // should only invoke once
+        if (this.isCalled) {
+            return;
+        }
+        this.isCalled = true;
+        if (this.resolveCb) {
+            setTimeout(() => this.resolveCb.apply(undefined, val));
+        }
+    }
+
+    callRejectAsync(val: any) {
+        // should only invoke once
+        if (this.isCalled) {
+            return;
+        }
+        this.isCalled = true;
+
+        if(this.rejectCb) {
+            setTimeout(() => this.rejectCb.apply(undefined, val));
+        }
+    }
 }
 
 export class Wish {
-    private state = State.Pending;
+    private state: State = State.Pending;
+    private subscribers: Subscriber[] = [];
 
     constructor(func: Executor) {
-        this.resolve.bind(this);
-        this.reject.bind(this);
+        this.resolve = this.resolve.bind(this);
+        this.reject = this.reject.bind(this);
         func.apply(undefined, [this.resolve, this.reject]);
     }
 
-    then() {
-
+    then(success?: CallBack, error?: CallBack) {
+        this.subscribers.push(new Subscriber(success, error));
     }
 
-    resolve() {
+    resolve(val: any) {
+        if (val === this) {
+            this.reject(TypeError("Can't resolve with self!"));
+            return;
+        }
 
+        // if resolved with a promise (wish)
+        if (val instanceof Wish) {
+
+            return;
+        }
+
+        // if resolved with a promise like object
+        if (val.then) {
+
+            return;
+        }
+
+        this.state = State.Fullfilled;
+
+        // resolve directly
+        for (const sub of this.subscribers) {
+            sub.callResolveAsync(val);
+        }
     }
 
-    reject() {
+    reject(val: any) {
 
     }
 };
